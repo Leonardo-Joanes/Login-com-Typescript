befcb8d99f8e380743a30c324f312fc813f2deab
Leonardo Joanes <leonardojoanes@gmail.com>
2023-08-06 01:21:34 -0300

    Adicionando rotas no App.tsx

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import './App.css';
+import { Route, Routes, Link } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { Private } from './pages/Private';
 
 function App() {
   return (
-    <>
-      <h1>Começando do Zero!</h1>
-    </>
+    <div>
+      <header>
+        <h1>Começando do Zero!</h1>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/private">Página Privada</Link>
+        </nav>
+      </header>
+      <hr />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/private" element={<Private />} />
+      </Routes>
+    </div>
   );
 }
 
